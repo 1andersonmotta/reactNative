38371a108dbb79edc66a53bfd3ae0c51f7847362
1andersonmotta <devandersonmotta@gmail.com>
2024-03-08 18:19:51 -0300

    add stack navigator

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Register from './src/assets/components/Register/Register.jsx';
 import Home from './App1.js';
+import { Feather } from '@expo/vector-icons';
 
 
 function HomeScreen() {
@@ -22,6 +24,16 @@ function SettingsScreen() {
         </View>
     );
 }
+function StackNavigator() {
+    return (
+        <StackNavigator screenOptions={{ title: '' }}>
+            <Stack.Screen
+                name='Home'
+                component={<Register />}
+            />
+        </StackNavigator>
+    );
+}
 
 function RegisterScreen() {
     return (
@@ -32,14 +44,44 @@ function RegisterScreen() {
 }
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
+        // <StackNavigator screenOptions={{ title: '' }}>
+        //     <Stack.Screen
+        //         name='Home'
+        //         component={<Register />}
+        //     />
+        // </StackNavigator>
+
+
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Serviços" component={SettingsScreen} />
-                <Tab.Screen name="Cadastrar" component={RegisterScreen} />
+                <Tab.Screen
+                    name="Início"
+                    component={HomeScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => <Feather name='home' color={color} size={size} />,
+                        tabBarLabel: 'Início'
+                    }}
+                />
+                <Tab.Screen
+                    name="Serviços"
+                    component={SettingsScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => <Feather name='tool' color={color} size={size} />,
+                        tabBarLabel: 'Serviços',
+                    }}
+                />
+                <Tab.Screen
+                    name="Cadastrar"
+                    component={RegisterScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => <Feather name='plus' color={color} size={size} />,
+                        tabBarLabel: 'Cadastrar'
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     );
